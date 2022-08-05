f90ba3c5b1977ec077fa16378f014a75a0df25ea
Femi Laizer <femilaizer@gmail.com>
2022-08-05 17:16:51 +0100

    new folders added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Services from './components/services/services'
+import Portfolio from './components/portfolio/portfolio'
+import Testimonials from './components/testimonials/testimonial'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <Contact />
+            <Footer />
+        </div>
     )
 }
 
